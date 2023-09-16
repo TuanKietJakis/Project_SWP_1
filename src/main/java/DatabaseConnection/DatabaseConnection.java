c1417edd1beadfde82749907f40bb9c378ca4e3a
TuanKietJakis <trantuankietzna153410@gmail.com>
2023-09-17 00:12:03 +0700

    lan 2

--- src/main/java/DatabaseConnection/DatabaseConnection.java
+++ src/main/java/DatabaseConnection/DatabaseConnection.java
@@ -4,10 +4,25 @@
  */
 package DatabaseConnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Kiet
  */
 public class DatabaseConnection {
+    private static Connection conn = null;
     
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        conn = DriverManager.getConnection("jdbc:sqlserver://HAIDANG:1433;"
+                    + "databaseName=PerfumeShop;"
+                    + "user=sa;password=123456;"
+                    + "encrypt=true;"
+                    + "trustServerCertificate=true;"
+                    + "sendStringParametersAsUnicode=true;");
+
+        return conn;
+    }
 }
